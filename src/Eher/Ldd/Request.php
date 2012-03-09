de43ff9fe02b616f198f3beb911c892c8ff74060
Eher <alexandre.eher@apontador.com>
2012-03-09 09:01:27 -0300

    Adds protocol and hostname do request.

--- src/Eher/Ldd/Request.php
+++ src/Eher/Ldd/Request.php
@@ -5,8 +5,11 @@ namespace Eher\Ldd;
 class Request
 {
     private $verb;
+    private $protocol;
+    private $hostname;
     private $path;
     private $parameters;
+
     public function __construct($request = null, $parameters = null)
     {
         if ($request != null) {
@@ -21,6 +24,9 @@ class Request
     public function proccessRequest($request)
     {
         $this->verb = strtolower($request['REQUEST_METHOD']);
+        list($protocolName, $protocolVersion) = explode("/", $request['SERVER_PROTOCOL']);
+        $this->protocol = strtolower($protocolName);
+        $this->hostname = strtolower($request['HTTP_HOST']);
         $this->path = strtolower($request['PATH_INFO']);
     }
 
@@ -38,6 +44,8 @@ class Request
     {
         return array(
             "verb" => $this->verb,
+            "protocol" => $this->protocol,
+            "hostname" => $this->hostname,
             "path" => $this->path,
             "parameters" => $this->parameters,
         );
--- test/Eher/Ldd/RequestTest.php
+++ test/Eher/Ldd/RequestTest.php
@@ -20,11 +20,13 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     {
         $request = array(
             "REQUEST_METHOD" => "GET",
+            "SERVER_PROTOCOL" => "HTTP/1.1",
+            "HTTP_HOST" => "eher.com.br",
             "PATH_INFO" => "/test/path",
         );
         $this->request->proccessRequest($request);
         $this->assertEquals(
-            '{"verb":"get","path":"\/test\/path","parameters":null}',
+            '{"verb":"get","protocol":"http","hostname":"eher.com.br","path":"\/test\/path","parameters":null}',
             $this->request->toJson()
         );
     }
@@ -33,6 +35,8 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     {
         $request = array(
             "REQUEST_METHOD" => "GET",
+            "SERVER_PROTOCOL" => "HTTP/1.1",
+            "HTTP_HOST" => "eher.com.br",
             "PATH_INFO" => "/test/path",
         );
         $parameters = array(
@@ -41,7 +45,7 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->request->proccessRequest($request);
         $this->request->proccessParameters($parameters);
         $this->assertEquals(
-            '{"verb":"get","path":"\/test\/path","parameters":{"eher":"test"}}',
+            '{"verb":"get","protocol":"http","hostname":"eher.com.br","path":"\/test\/path","parameters":{"eher":"test"}}',
             $this->request->toJson()
         );
     }
