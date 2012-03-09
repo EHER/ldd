c74cf3494d40792109f55204add4a10b6a81584e
Eher <alexandre.eher@apontador.com>
2012-03-09 17:08:40 -0300

    Bugfix: request url

--- src/Eher/Ldd/Request.php
+++ src/Eher/Ldd/Request.php
@@ -43,7 +43,7 @@ class Request
             $client = new Client();
         }
 
-        $url = $this->protocol . '://' . $this->hostname . '/' . $this->path;
+        $url = $this->protocol . '://' . $this->hostname . $this->path;
 
         switch($this->verb) {
         case 'get':
